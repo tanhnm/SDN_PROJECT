2485a65b3cf69c00a4f175b57aedcb5b29b75c06
tanhnm <hnmtan03@gmail.com>
2024-07-08 12:54:52 +0700

    Adding handle expired tokens

--- src/Components/Molescule/LoginForm/LoginForm.jsx
+++ src/Components/Molescule/LoginForm/LoginForm.jsx
@@ -1,14 +1,14 @@
-import PetDog from "../../../assets/images/PetDog.png";
+import PetDog from '../../../assets/images/PetDog.png';
 
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import MyAxios from "../../../setup/configAxios";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import MyAxios from '../../../setup/configAxios';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const initFormValue = {
-  email: "",
-  password: "",
+  email: '',
+  password: '',
 };
 
 // Check empty
@@ -29,26 +29,26 @@ const LoginForm = () => {
 
   // Xử lý remember me
   useEffect(() => {
-    const savedUser = localStorage.getItem("user");
-    const savedRememberMe = localStorage.getItem("rememberMe");
+    const savedUser = localStorage.getItem('user');
+    const savedRememberMe = localStorage.getItem('rememberMe');
 
     if (savedUser) {
       try {
         const user = JSON.parse(savedUser);
         setFormValue({
           email: user.email,
-          password: "",
+          password: '',
         });
 
-        if (savedRememberMe === "true") {
+        if (savedRememberMe === 'true') {
           setRememberMe(true);
         } else {
           setRememberMe(false);
         }
       } catch (error) {
         console.error(error);
-        localStorage.removeItem("user");
-        localStorage.removeItem("rememberMe");
+        localStorage.removeItem('user');
+        localStorage.removeItem('rememberMe');
       }
     }
   }, []);
@@ -70,33 +70,34 @@ const LoginForm = () => {
   // Post API
   const loginUser = async (email, password) => {
     try {
-      const response = await MyAxios.post("http://localhost:5000/api/v1/auth/login", {
+      const response = await MyAxios.post('http://localhost:5000/api/v1/auth/login', {
         email,
         password,
       });
+      console.log(response);
       const { access_token, refresh_token } = response.data;
 
-      if (response.status === "success") {
+      if (response.status === 'success') {
         if (rememberMe) {
           saveLoginInfo(access_token, refresh_token);
         }
-        localStorage.setItem("access_token", response.data.access_token);
-        localStorage.setItem("refresh_token", response.data.refresh_token);
-        localStorage.setItem("userId", response.data.id);
-        localStorage.setItem("userRole", response.data.role);
+        localStorage.setItem('access_token', response.data.access_token);
+        localStorage.setItem('refresh_token', response.data.refresh_token);
+        localStorage.setItem('userId', response.data.id);
+        localStorage.setItem('userRole', response.data.role);
 
-        const userRole = localStorage.getItem("userRole");
-        if (userRole === "STAFF") {
-          window.location.href = "/staff";
-        } else if (userRole === "CUSTOMER") {
-          window.location.href = "/";
+        const userRole = localStorage.getItem('userRole');
+        if (userRole === 'STAFF') {
+          window.location.href = '/staff';
+        } else if (userRole === 'CUSTOMER') {
+          window.location.href = '/';
         } else {
           // Handle other roles or unexpected cases
-          console.log("Unknown user role:", userRole);
+          console.log('Unknown user role:', userRole);
         }
       } else {
         setLoginFail(true);
-        toast.error(`Đăng nhập thất bại: ${response.message || "Unknown error"}`);
+        toast.error(`Đăng nhập thất bại: ${response.message || 'Unknown error'}`);
       }
     } catch (error) {
       setLoginFail(true); // Cập nhật trạng thái đăng nhập thất bại
@@ -107,22 +108,22 @@ const LoginForm = () => {
   const validateForm = () => {
     const errors = {};
     if (isEmptyValue(formValue.email)) {
-      errors.email = "Email is required";
+      errors.email = 'Email is required';
     } else if (!isEmailValid(formValue.email)) {
-      errors.email = "Email is invalid";
+      errors.email = 'Email is invalid';
     }
     if (isEmptyValue(formValue.password)) {
-      errors.password = "Password is required";
+      errors.password = 'Password is required';
     }
     setFormError(errors);
     return Object.keys(errors).length === 0;
   };
 
   const saveLoginInfo = (user, access_token, refresh_token) => {
-    localStorage.setItem("user", JSON.stringify(user));
-    localStorage.setItem("access_token", access_token);
-    localStorage.setItem("refresh_token", refresh_token);
-    localStorage.setItem("rememberMe", rememberMe);
+    localStorage.setItem('user', JSON.stringify(user));
+    localStorage.setItem('access_token', access_token);
+    localStorage.setItem('refresh_token', refresh_token);
+    localStorage.setItem('rememberMe', rememberMe);
   };
 
   // Xử lý đăng nhập
@@ -131,84 +132,88 @@ const LoginForm = () => {
     if (validateForm()) {
       loginUser(formValue.email, formValue.password);
     } else {
-      console.log("Form invalid");
+      console.log('Form invalid');
     }
   };
 
   return (
     <>
-      <div className="w-full h-screen flex items-start">
-        <div className="relative w-1/2 h-full flex flex-col">
-          <img src={PetDog} className="w-full h-full" alt="Pet Dog" />
+      <div className='w-full h-screen flex items-start'>
+        <div className='relative w-1/2 h-full flex flex-col'>
+          <img src={PetDog} className='w-full h-full' alt='Pet Dog' />
         </div>
-        <div className="w-1/2 h-full bg-[#fff] flex flex-col p-20 justify-between items-center">
-          <h1 className="w-full max-w-[550px] mx-auto text-xl text-[#060606] font-semibold mr-auto">
+        <div className='w-1/2 h-full bg-[#fff] flex flex-col p-20 justify-between items-center'>
+          <h1 className='w-full max-w-[550px] mx-auto text-xl text-[#060606] font-semibold mr-auto'>
             Pet Home dịch vụ tốt nhất cho thú cưng
           </h1>
-          <div className="w-full flex flex-col max-w-[550px]">
-            <div className="w-full flex flex-col mb-2">
-              <h3 className="text-4xl font-semibold mb-2">Login</h3>
-              <p className="text-base mb-2">Welcome Back! Please fill your details.</p>
+          <div className='w-full flex flex-col max-w-[550px]'>
+            <div className='w-full flex flex-col mb-2'>
+              <h3 className='text-4xl font-semibold mb-2'>Login</h3>
+              <p className='text-base mb-2'>Welcome Back! Please fill your details.</p>
             </div>
-            <form className="w-full flex flex-col" onSubmit={handleLogin}>
+            <form className='w-full flex flex-col' onSubmit={handleLogin}>
               <input
-                type="email"
-                name="email"
+                type='email'
+                name='email'
                 value={formValue.email}
                 onChange={handleChange}
-                placeholder="Email"
-                className="w-full text-black py-2 my-2 bg-transparent border-b border-black outline-none focus:outline-none"
+                placeholder='Email'
+                className='w-full text-black py-2 my-2 bg-transparent border-b border-black outline-none focus:outline-none'
                 required
-                autoComplete="email"
+                autoComplete='email'
               />
-              {formError.email && <div className="text-red-500 text-sm">{formError.email}</div>}
+              {formError.email && <div className='text-red-500 text-sm'>{formError.email}</div>}
               <input
-                type="password"
-                name="password"
+                type='password'
+                name='password'
                 value={formValue.password}
                 onChange={handleChange}
-                placeholder="Password"
-                className="w-full text-black py-2 my-2 bg-transparent border-b border-black outline-none focus:outline-none"
+                placeholder='Password'
+                className='w-full text-black py-2 my-2 bg-transparent border-b border-black outline-none focus:outline-none'
                 required
-                autoComplete="current-password"
+                autoComplete='current-password'
               />
-              {formError.password && <div className="text-red-500 text-sm">{formError.password}</div>}
-              <div className="w-full flex items-center justify-between">
-                <div className="w-full flex items-center">
+              {formError.password && (
+                <div className='text-red-500 text-sm'>{formError.password}</div>
+              )}
+              <div className='w-full flex items-center justify-between'>
+                <div className='w-full flex items-center'>
                   <input
-                    type="checkbox"
+                    type='checkbox'
                     checked={rememberMe}
                     onChange={handleRememberMeChange}
-                    className="w-4 h-4 mr-2"
+                    className='w-4 h-4 mr-2'
                   />
-                  <p className="text-sm">Remember me</p>
+                  <p className='text-sm'>Remember me</p>
                 </div>
-                <p className="text-sm font-medium whitespace-nowrap cursor-pointer underline underline-offset-2">
+                <p className='text-sm font-medium whitespace-nowrap cursor-pointer underline underline-offset-2'>
                   Forgot Password?
                 </p>
               </div>
-              <div className="w-full flex flex-col my-4">
+              <div className='w-full flex flex-col my-4'>
                 <button
-                  type="submit"
-                  className="w-full text-white my-2 bg-[#060606] rounded-md p-4 text-center flex items-center justify-center cursor-pointer"
+                  type='submit'
+                  className='w-full text-white my-2 bg-[#060606] rounded-md p-4 text-center flex items-center justify-center cursor-pointer'
                 >
                   Log in
                 </button>
-                <button className="w-full text-[#060606] my-2 bg-white border-2 border-black rounded-md p-4 text-center flex items-center justify-center cursor-pointer">
+                <button className='w-full text-[#060606] my-2 bg-white border-2 border-black rounded-md p-4 text-center flex items-center justify-center cursor-pointer'>
                   Register
                 </button>
               </div>
             </form>
-            <div className="w-full flex items-center justify-center relative py-2">
-              <div className="w-full h-[1px] bg-black"></div>
-              <p className="text-lg absolute text-black/80 bg-[#f5f5f5] px-2">or</p>
+            <div className='w-full flex items-center justify-center relative py-2'>
+              <div className='w-full h-[1px] bg-black'></div>
+              <p className='text-lg absolute text-black/80 bg-[#f5f5f5] px-2'>or</p>
             </div>
           </div>
-          <div className="w-full flex items-center justify-center">
-            <p className="text-sm font-normal text-black">
-              Don't have a account ?{" "}
-              <Link to="/register">
-                <span className="font-semibold underline underline-offset-2 cursor-pointer">Sign up for free ?</span>
+          <div className='w-full flex items-center justify-center'>
+            <p className='text-sm font-normal text-black'>
+              Don't have a account ?{' '}
+              <Link to='/register'>
+                <span className='font-semibold underline underline-offset-2 cursor-pointer'>
+                  Sign up for free ?
+                </span>
               </Link>
             </p>
           </div>
--- src/Components/layout/Header/HeaderAfterLogin.jsx
+++ src/Components/layout/Header/HeaderAfterLogin.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext, useEffect } from 'react';
 import {
   AppBar,
   Box,
@@ -9,23 +9,34 @@ import {
   Toolbar,
   Typography,
   Badge,
-} from "@mui/material";
-import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
-import Avatar from "@mui/material/Avatar";
-import HomeIcon from "@mui/icons-material/Home";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import SearchIcon from "@mui/icons-material/Search";
-import { Link, useNavigate } from "react-router-dom";
-import "./HeaderAfterLogin.scss";
-import { CartContext } from "context/CartContext";
-import MyAxios from "setup/configAxios";
+} from '@mui/material';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import Avatar from '@mui/material/Avatar';
+import HomeIcon from '@mui/icons-material/Home';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import SearchIcon from '@mui/icons-material/Search';
+import { Link, useNavigate } from 'react-router-dom';
+import './HeaderAfterLogin.scss';
+import { CartContext } from 'context/CartContext';
+import MyAxios from 'setup/configAxios';
+import useTokenExpiration from 'hooks/useAuthExpired';
 function HeaderAfterLogin() {
+  const token = localStorage.getItem('access_token'); // or wherever you store your token
+  const isTokenExpired = useTokenExpiration(token);
   const [anchorEl, setAnchorEl] = useState(null);
   const [searchExpanded, setSearchExpanded] = useState(false);
-  const [searchText, setSearchText] = useState("");
+  const [searchText, setSearchText] = useState('');
   const { cartItem } = useContext(CartContext);
   const navigate = useNavigate();
   const [userInfo, setUserInfo] = useState();
+
+  if (isTokenExpired) {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('refresh_token');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('userRole');
+    navigate('/login');
+  }
 
   const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
@@ -44,7 +55,7 @@ function HeaderAfterLogin() {
   };
 
   const handleSearchKeyPress = (event) => {
-    if (event.key === "Enter") {
+    if (event.key === 'Enter') {
       event.preventDefault();
       if (searchText) {
         navigate(`/product/search?query=${searchText}`);
@@ -56,105 +67,100 @@ function HeaderAfterLogin() {
   // Calculate total items in the cart
   const totalCartItems = cartItem.reduce((acc, item) => acc + item.value, 0);
 
-  const access_token = localStorage.getItem("access_token");
+  const access_token = localStorage.getItem('access_token');
 
   const getUserInfo = () => {
-    const userId = localStorage.getItem("userId");
+    const userId = localStorage.getItem('userId');
     MyAxios.get(`http://localhost:5000/api/v1/user/${userId}`).then((res) => {
       setUserInfo(res.data);
     });
   };
 
   const handleLogout = () => {
-    localStorage.removeItem("access_token");
-    localStorage.removeItem("refresh_token");
-    localStorage.removeItem("userId");
-    localStorage.removeItem("userRole");
-    navigate("/login");
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('refresh_token');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('userRole');
+    navigate('/login');
   };
 
   return (
-    <Box sx={{ flexGrow: 1 }} className="header-container">
-      <AppBar position="static">
-        <Toolbar className="header">
-          <div className="header-info">
-            <div className="header-phone">
-              <LocalPhoneIcon fontSize="large" className={"header-color"} />
-              <Typography className="header-color">09123678754</Typography>
+    <Box sx={{ flexGrow: 1 }} className='header-container'>
+      <AppBar position='static'>
+        <Toolbar className='header'>
+          <div className='header-info'>
+            <div className='header-phone'>
+              <LocalPhoneIcon fontSize='large' className={'header-color'} />
+              <Typography className='header-color'>09123678754</Typography>
             </div>
-            <div className="header-address">
-              <HomeIcon fontSize="large" className={"header-color"} />
-              <Typography className="header-color">
+            <div className='header-address'>
+              <HomeIcon fontSize='large' className={'header-color'} />
+              <Typography className='header-color'>
                 123 Nguyễn Trãi, P.Tân Phú, Q.3, TPHCM
               </Typography>
               {searchExpanded && (
-                <div className="header-search">
+                <div className='header-search'>
                   <input
-                    type="text"
+                    type='text'
                     value={searchText}
                     onChange={handleSearchChange}
                     onKeyPress={handleSearchKeyPress}
-                    placeholder="Search..."
-                    className="header-search-input"
+                    placeholder='Search...'
+                    className='header-search-input'
                   />
                 </div>
               )}
-              <IconButton
-                onClick={handleSearchClick}
-                className={"header-color"}
-              >
-                <SearchIcon className={"header-color"} />
+              <IconButton onClick={handleSearchClick} className={'header-color'}>
+                <SearchIcon className={'header-color'} />
               </IconButton>
             </div>
           </div>
-          <Link to="/shopping-cart" className="header-cart">
-            <Badge badgeContent={totalCartItems} color="secondary">
-              <ShoppingCartIcon fontSize="large" className={"header-color"} />
+          <Link to='/shopping-cart' className='header-cart'>
+            <Badge badgeContent={totalCartItems} color='secondary'>
+              <ShoppingCartIcon fontSize='large' className={'header-color'} />
             </Badge>
           </Link>
 
           {access_token ? (
             <>
-              {" "}
-              <Stack direction="row" spacing={4}>
-                <Stack direction="row" alignItems={"center"} spacing={1}>
+              {' '}
+              <Stack direction='row' spacing={4}>
+                <Stack direction='row' alignItems={'center'} spacing={1}>
                   <IconButton onClick={handleMenu} sx={{ p: 0 }}>
                     <Avatar
-                      alt="User Avatar"
+                      alt='User Avatar'
                       src={
-                        "https://t4.ftcdn.net/jpg/04/22/57/65/360_F_422576509_8MxGhSGZ4otQPtV6FyqO2FPrgNRTlEXj.jpg"
+                        'https://t4.ftcdn.net/jpg/04/22/57/65/360_F_422576509_8MxGhSGZ4otQPtV6FyqO2FPrgNRTlEXj.jpg'
                       }
                     />
                   </IconButton>
-                  <Typography variant="subtitle2" className="text-white">
+                  <Typography variant='subtitle2' className='text-white'>
                     {/* {userInfo.name} */}
                   </Typography>
                 </Stack>
               </Stack>
               <Menu
-                id="menu-appbar"
+                id='menu-appbar'
                 anchorEl={anchorEl}
                 anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
+                  vertical: 'top',
+                  horizontal: 'right',
                 }}
                 keepMounted
                 transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
+                  vertical: 'top',
+                  horizontal: 'right',
                 }}
                 open={Boolean(anchorEl)}
                 onClose={handleClose}
               >
-                <MenuItem onClick={() => navigate("/profile")}>Hồ sơ</MenuItem>
-                <MenuItem onClick={() => navigate("/pet-info")}>
-                  Danh sách thú cưng
-                </MenuItem>
+                <MenuItem onClick={() => navigate('/profile')}>Hồ sơ</MenuItem>
+                <MenuItem onClick={() => navigate('/pet-info')}>Danh sách thú cưng</MenuItem>
                 <MenuItem onClick={handleLogout}>Log Out</MenuItem>
               </Menu>
             </>
           ) : (
-            <button onClick={() => navigate("/login")}>Login</button>
+            <button onClick={() => navigate('/login')}>Login</button>
           )}
         </Toolbar>
       </AppBar>
