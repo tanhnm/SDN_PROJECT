917a357b110f0221ecdbe04e619f903526126c79
tanhnm <hnmtan03@gmail.com>
2024-07-11 00:53:39 +0700

    Adding handle bug Modal and adding color primary for pages config refresh token for axiosClient

--- src/setup/configAxios.js
+++ src/setup/configAxios.js
@@ -1,20 +1,18 @@
-import axios from "axios";
+import axios from 'axios';
 
 // Set config defaults when creating the instance
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: 'http://localhost:5000',
 });
 instance.defaults.withCredentials = true;
-// Alter defaults after instance has been created
-let access_token = localStorage.getItem("access_token");
-if (access_token) {
-  let token = [];
-  token = access_token;
-  instance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-}
+
 // Add a request interceptor
 instance.interceptors.request.use(
   function (config) {
+    const access_token = localStorage.getItem('access_token');
+    if (access_token) {
+      config.headers['Authorization'] = `Bearer ${access_token}`;
+    }
     // Do something before request is sent
     return config;
   },
@@ -24,16 +22,44 @@ instance.interceptors.request.use(
   }
 );
 
+const refreshToken = async () => {
+  const refresh_token = localStorage.getItem('refresh_token');
+  if (!refresh_token) {
+    window.location.href = '/login';
+  }
+  const response = await axios.post('http://localhost:5000/api/v1/auth/refreshToken', {
+    refresh_token,
+  });
+
+  const { access_token } = response.data.data;
+  localStorage.setItem('access_token', access_token);
+  return access_token;
+};
+
 // Add a response interceptor
 instance.interceptors.response.use(
   function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    return response.data;
+    return response;
   },
-  function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
+  async function (error) {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const newAccessToken = await refreshToken();
+        if (newAccessToken) {
+          originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+          return instance(originalRequest);
+        }
+      } catch (error) {
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('refresh_token');
+        localStorage.removeItem('userId');
+        localStorage.removeItem('userRole');
+        window.location.href = '/login';
+        return Promise.reject(error);
+      }
+    }
     return Promise.reject(error);
   }
 );
